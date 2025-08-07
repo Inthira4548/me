c4d3dd2f0965af6ce36e4d2c1c2a7e238fb51714
Inthira4548 <kineqwasg@gmail.com>
2025-08-07 09:19:03 +0700

    Add files via upload

---
+++ SQLQuery1.sql
@@ -0,0 +1,14 @@
+select * from Products
+where UnitPrice = 15;
+
+select * from Products
+where UnitsInStock  < 250;
+
+SELECT ProductID, ProductName FROM Product
+WHERE Discontinued = 1;
+
+
+SELECT ProductID, ProductName, Price FROM Product
+WHERE Price > 100;
+
+
