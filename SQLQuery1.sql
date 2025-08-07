ef2ef28640416d00de5f8745e4e4f456a0071c5c
Inthira4548 <kineqwasg@gmail.com>
2025-08-07 10:13:50 +0700

    Add files via upload

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -11,53 +11,55 @@ where UnitsInStock  < 250;
 select 
 * from Products where Discontinued = 1
 -- 4. รหัสสินค้า ชื่อสินค้า ราคา ของสินค้าที่มีราคามากกว่า 100
-select 
-* from ProductID where Discontinued = 1
+SELECT ProductID, ProductName, UnitPrice FROM Products
+WHERE UnitPrice > 100;
+
+
+
 -- 5. รหัสสินค้า และราคาของยางลบ
-SELECT ProductID, Price FROM Product
-WHERE ProductName LIKE '%eraser%';
+SELECT ProductID,UnitPrice FROM Products
+WHERE ProductName LIKE '%ยางลบ%';
+
 -- 6. หมายเลขใบเสร็จ วันที่ และ ราคารวม ของใบเสร็จที่ออกก่อนวันที่ 15 ก.พ.
-SELECT ReceiptID, ReceiptDate, TotalPrice FROM Receipt
+SELECT ReceiptID, ReceiptDate, TotalCash FROM Receipts
 WHERE ReceiptDate < '2025-02-15';
+
 -- 7. รหัสสินค้า ชื่อสินค้า ที่มีจำนวนคงเหลือตั้งแต่ 400 ขึ้นไป
-SELECT ProductID, ProductName FROM Product
-WHERE StockQty >= 400;
+
+SELECT ProductID, ProductName FROM Products
+WHERE UnitsInStock >= 400;
 
 -- 8. รหัสสินค้า ชื่อสินค้า ราคา และ จำนวนคงเหลือ ของแชมพู,แป้งเด็ก,ดินสอ,ยางลบ
 
-SELECT ProductID, ProductName, Price, StockQty FROM Product
+SELECT ProductID, ProductName, UnitPrice, UnitsInStock FROM Products
 WHERE ProductName IN ('แชมพู', 'แป้งเด็ก', 'ดินสอ', 'ยางลบ');
 
 
 -- 9. รายละเอียดของสินค้าประเภทเครื่องเขียน
-SELECT * FROM Product
-WHERE ProductTypeID = (
-  SELECT ProductTypeID FROM ProductType
-  WHERE ProductTypeName = 'เครื่องเขียน'
-);
+SELECT [Description] from  Categories where CategoryName = 'เครื่องเขียน'
+
 
 -- 10. รหัสประเภทสินค้า ชื่อประเภท และรายละเอียดของ สินค้าประเภทเครื่องสำอาง
-SELECT ProductTypeID, ProductTypeName, Description FROM ProductType
-WHERE ProductTypeName = 'เครื่องสำอาง';
+SELECT CategoryID , CategoryName,[Description] from  Categories 
 
 -- 11.คำนำหน้า ชื่อ นามสกุล ของพนักงานที่เป็น Sale Representative
-SELECT Title, FirstName, LastName FROM Employee
+SELECT Title, FirstName, LastName FROM Employees
 WHERE Position = 'Sale Representative';
 
 -- 12. รหัสพนักงาน ชื่อพนักงาน ชื่อผู้ใช้ รหัสผ่าน ของพนักงานทุกคน
-SELECT EmployeeID, FirstName, LastName, Username, Password FROM Employee;
+SELECT EmployeeID, FirstName, LastName, Username, Password FROM Employees
 
 -- 13. ชื่อผู้ใช้ และรหัสผ่านของพนักงานที่ชื่อก้องนิรันดร์
-SELECT Username, Password FROM Employee
+SELECT Username, Password FROM Employees
 WHERE FirstName = 'ก้องนิรันดร์';
 
 -- 14. รหัสพนักงานที่ออกใบเสร็จหมายเลข 3
-SELECT EmployeeID FROM Receipt
+SELECT EmployeeID FROM Receipts
 WHERE ReceiptID = 3;
 
 -- 15. รหัสสินค้า ชื่อสินค้า ราคา ของสินค้าที่มีรหัสประเภท 2, 4
-SELECT ProductID, ProductName, Price FROM Product
-WHERE ProductTypeID IN (2, 4);
+SELECT ProductID, ProductName, UnitPrice FROM Products
+WHERE CategoryID  IN (2, 4);
 
 
 
